6afbfd62778a89a766296d0be484fdfc77dd4b55
AI-Jiny <key140@naver.com>
2020-07-21 20:01:03 +0900

    문제풀이

---
+++ Problem/03_for문/02_A+B-3.py
@@ -0,0 +1,5 @@
+a = int(input())
+for i in range(a):
+    b = input().split()
+    s = [int(t) for t in b]
+    print(s[0] + s[1])
---
+++ Problem/03_for문/03_합.py
@@ -0,0 +1,5 @@
+a = int(input())
+num = 0
+for i in range(a+1):
+    num += i
+print(num)
