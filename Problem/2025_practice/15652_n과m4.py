8bfbf71c6ca591daa0d7aac64d04ba8bb9e23d1c
AI-Jiny <key140@naver.com>
2025-12-30 12:29:44 +0900

    [코테] n과m

---
+++ Problem/2025_practice/15652_n과m4.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+path = []
+
+def dfs(start):
+    if len(path) == M:
+        print(*path)
+        return
+    
+    for i in range(start, N + 1):
+        path.append(i)
+        dfs(i)
+        path.pop()
+
+dfs(1)
