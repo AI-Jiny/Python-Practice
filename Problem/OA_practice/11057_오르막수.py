d43b85df33c145d8a40b8997c4aa5465e8461a7c
AI-Jiny <key140@naver.com>
2026-02-10 10:31:52 +0900

    [코테]오르막수

---
+++ Problem/OA_practice/11057_오르막수.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+dp = [[0 for _ in range(10)] for _ in range(n + 1)]
+
+for i in range(10):
+    dp[1][i] = 1
+
+
+for i in range(1, n + 1):
+    dp[i][0] = 1
+    for j in range(1, 10):
+        dp[i][j] = (dp[i][j - 1] + dp[i -1][j]) % 10007
+
+ans = (sum(dp[n]) % 10007)
+print(ans)
