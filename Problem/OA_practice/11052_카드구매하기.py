68efe625a34f621ec773b1ad0f62f3ae97d807ff
AI-Jiny <key140@naver.com>
2026-02-18 22:14:59 +0900

    [코테]카드 구매하기

---
+++ Problem/OA_practice/11052_카드구매하기.py
@@ -0,0 +1,10 @@
+n = int(input())
+p_list = map(int, input().split())
+dp = [0 for _ in range(n + 1)]
+for i, p in enumerate(p_list, 1):
+    for j in range(i, n + 1):
+        dp[j] = max(dp[j], dp[j - i] + p)
+
+print(dp)
+# 10
+# 1 1 2 3 5 8 13 21 34 55
