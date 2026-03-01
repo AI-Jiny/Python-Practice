90a87a300daffab82ba6fcaa2f68f54524de51a6
AI-Jiny <key140@naver.com>
2026-03-01 14:31:34 +0900

    [코테]택배 배송

---
+++ Problem/OA_practice/5972_택배배송(re).py
@@ -0,0 +1,42 @@
+import sys
+from math import inf
+import heapq
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = {i: [] for i in range(n + 1)}
+dp = [inf] * (n + 1)
+dp[1] = 0
+hq = []
+heapq.heappush(hq, (0, 1))
+
+for _ in range(m):
+    s, e, c = map(int, input().split())
+    graph[s].append((c, e))
+    graph[e].append((c, s))
+
+while hq:
+    cost, cur = heapq.heappop(hq)
+    if cost > dp[cur]:
+        continue
+    
+    for g in graph[cur]:
+        tc, nxt = g
+        ncost = tc + cost
+        if ncost < dp[nxt]:
+            dp[nxt] = ncost
+            heapq.heappush(hq, (ncost, nxt))
+
+print(graph)
+print(dp)
+
+# 6 8
+# 4 5 3
+# 2 4 0
+# 4 1 4
+# 2 1 1
+# 5 6 1
+# 3 6 2
+# 3 2 6
+# 3 4 4
