189c47d7a5510c2e7b4882e8d763fd8bff3a2739
AI-Jiny <key140@naver.com>
2026-03-02 18:10:30 +0900

    [코테] 용액(re)

---
+++ Problem/OA_practice/2467_용액(re).py
@@ -0,0 +1,24 @@
+n = int(input())
+l = [int(x) for x in input().split()]
+left = 0
+right = n - 1
+ans_sum = 2000000000
+ans = (None, None)
+
+while left < right:
+    s = l[left] + l[right]
+    print(l[left], l[right])
+    if abs(s) < ans_sum:
+        ans_sum = abs(s)
+        ans = (l[left], l[right])
+   
+    if s < 0:
+        left += 1
+    elif s == 0:
+        break
+    else:
+        right -= 1    
+print(ans[0], ans[1])
+
+# 4
+# -100 -2 -1 103
