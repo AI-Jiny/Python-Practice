553b618595c47ae81b854a6678ad8d1e6eee1279
AI-Jiny <key140@naver.com>
2026-04-16 11:08:43 +0900

    [OA] 전구와 스위치(re)

---
+++ Problem/OA_practice/2138_전구와스위치(re).py
@@ -0,0 +1,30 @@
+N = int(input())
+start = [True if int(x) else False for x in input()]
+start_d = start[:]
+start_d[0] ^= True
+start_d[1] ^= True
+ans_1 = 0
+ans_2 = 1
+end = [x == '1' for x in input()]
+
+for i in range(1, N):
+    if start[i - 1] != end[i - 1]:
+        start[i - 1] ^= True
+        start[i] ^= True
+        if i + 1 < N:
+            start[i + 1] ^= True
+        ans_1 += 1
+    if start_d[i - 1] != end[i - 1]:
+        start_d[i - 1] ^= True
+        start_d[i] ^= True
+        if i + 1 < N:
+            start_d[i + 1] ^= True
+        ans_2 += 1
+
+if start[-1] == end[-1]:
+    print(ans_1)
+elif start_d[-1] == end[-1]:
+    print(ans_2)
+else:
+    print(-1)
+
